5a44a1b6674d51808bc0a101cd8ff850f576f50a
Fabio Ventura <foravatenubi@gmail.com>
2020-12-20 16:45:37 +0100

    add api.http

--- src/app/models/movement.model.ts
+++ src/app/models/movement.model.ts
@@ -2,13 +2,16 @@ export class Movement {
 
   public id: number;
   public date: Date;
+  public account: string;
   public amount: number;
   public description: string;
 
   constructor(obj?: any) {
-    this.id           = obj && obj.id           || null;
-    this.date         = obj && obj.date         || null;
-    this.amount       = obj && obj.amount       || null;
-    this.description  = obj && obj.description  || null;
+    this.id           = obj && obj.id || null;
+    this.date         = obj && obj.date || null;
+    this.account      = obj && obj.account || null;
+    this.amount       = obj && obj.amount || null;
+    this.description  = obj && obj.description || null;
   }
 }
+
